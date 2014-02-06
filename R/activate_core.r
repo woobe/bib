de04e1c4e6ba1599be9ac43b00bba3449d73c470
Jo-fai Chow <jofai.chow@gmail.com>
2014-02-06 15:09:17 +0000

    Added 'verbose = TRUE' option

--- R/activate_core.r
+++ R/activate_core.r
@@ -1,21 +1,18 @@
 #' Activate multiple CPU cores for parallel computing.
 
-activate_core <- function(num_core = 4) {
+activate_core <- function(num_core = 4, verbose = TRUE) {
   
-  ## Timer
-  tt <- start_timer()
+  if (verbose) tt <- start_timer()
   
   ## Load Packages
   suppressMessages(library(doSNOW))
   suppressMessages(library(foreach))  
-  
-  ## Display
-  cat("[bib]: Activaing parallel processing ... ")
+
+  if (verbose) cat("[bib]: Activaing parallel processing ... ")
   
   ## Activate Cores
   registerDoSNOW(makeCluster(num_core, type="SOCK"))
   
-  ## Display
-  cat(num_core, "cores have been successfully activated in", stop_timer(tt), "seconds.\n")
-  
+  if (verbose) cat(num_core, "cores have been successfully activated in", stop_timer(tt), "seconds.\n")
+    
 }
