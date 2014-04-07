2835d7d9ccb534fd6b31563b885537f9f279f7f4
Jo-fai Chow <jofai.chow@gmail.com>
2014-04-07 15:32:55 +0100

    Added packages dependency

--- R/install_key_pkg.r
+++ R/install_key_pkg.r
@@ -7,118 +7,94 @@ install_key_pkg <- function() {
   ## Key packages on CRAN
   ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
-  list_pkg <- c("devtools","caret","randomForest","e1071","kernlab","doSNOW",
-                    "knitr","doSNOW","Rserve","shiny","forecast","ffbase","gbm",
-                    "dplyr", "data.table","microbenchmark","ggmap","jsonlite",
-                    "roxygen2", "animation", "Quandl", "quantmod")
+  if (TRUE) {
   
-  for (num_pkg in 1:length(list_pkg)) {  
+    ## Edit this list manually
+    list_pkg <- c("devtools","caret","randomForest","e1071","kernlab","doSNOW",
+                  "knitr","doSNOW","Rserve","shiny","forecast","ffbase","gbm",
+                  "plyr", "dplyr", "data.table","microbenchmark","ggmap","jsonlite",
+                  "roxygen2", "animation", "Quandl", "quantmod")
     
-    ## Display
-    cat("Checking Package:", list_pkg[num_pkg], "...")
-    
-    ## Check
-    suppressMessages(temp_check <- require(list_pkg[num_pkg], 
-                                           character.only = TRUE,
-                                           warn.conflicts = FALSE,
-                                           quietly = TRUE))
-    
-    ## Install if needed
-    if (temp_check) {
-      cat(" already installed.\n")
-    } else {
-      cat(" not there ... now installing ...\n")
-      suppressMessages(install.packages(list_pkg[num_pkg]))
+    ## Install packages one by one
+    for (num_pkg in 1:length(list_pkg)) {  
+      
+      ## Display
+      cat("Checking Package:", list_pkg[num_pkg], "...")
+      
+      ## Check
+      suppressWarnings(
+        suppressMessages(temp_check <- require(list_pkg[num_pkg], 
+                                               character.only = TRUE,
+                                               warn.conflicts = FALSE,
+                                               quietly = TRUE)))
+      
+      
+      ## Install if needed
+      if (temp_check) {
+        cat(" already installed.\n")
+      } else {
+        cat(" not there ... now installing ...\n")
+        suppressMessages(install.packages(list_pkg[num_pkg]))
+      }
+      
     }
     
   }
   
   
   ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## Shinyapps
+  ## Install Packages on GitHub
   ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
-  cat("Checking Package: shinyapps ...")
+  ## Edit this list manually
+  list_pkg <- data.frame(matrix(NA, nrow = 1, ncol = 3))
+  colnames(list_pkg) <- c("author", "package", "dev")
+  list_pkg[1,] <- c("rstudio", "shinyapps", "no")
+  list_pkg[2,] <- c("ramnathv", "rCharts", "yes")
+  list_pkg[3,] <- c("ramnathv", "slidify", "yes")
+  list_pkg[4,] <- c("ramnathv", "slidifyLibraries", "yes")
+  list_pkg[5,] <- c("ramnathv", "rMaps", "no")
+  list_pkg[6,] <- c("ramnathv", "rblocks", "no")
+  list_pkg[7,] <- c("woobe", "rCrimemap", "no")
   
-  ## Check
-  suppressMessages(temp_check <- require("shinyapps", 
-                                         character.only = TRUE,
-                                         warn.conflicts = FALSE,
-                                         quietly = TRUE))
-  
-  ## Install if needed
-  if (temp_check) {
-    cat(" already installed.\n")
-  } else {
-    cat(" not there ... now installing ...\n")
-    suppressMessages(devtools::install_github('rstudio/shinyapps'))
+  ## Install packages one by one
+  for (n_pkg in 1:nrow(list_pkg)) {
+    
+    ## Display
+    cat("Checking Package:", list_pkg[n_pkg,]$author, "/", list_pkg[n_pkg,]$package, "...")
+    
+    ## Check
+    suppressWarnings(
+      suppressMessages(temp_check <- 
+                         require(list_pkg[n_pkg,]$package, 
+                                 character.only = TRUE,
+                                 warn.conflicts = FALSE,
+                                 quietly = TRUE)))
+    
+    ## Install if needed
+    if (!temp_check) {
+      
+      ## Display
+      cat(" not there ... now installing ...\n")
+      
+      ## Generate text
+      txt_install <- paste0(list_pkg[n_pkg,]$author, "/", list_pkg[n_pkg,]$package)
+      
+      ## Add "@dev" if needed
+      if (list_pkg[n_pkg,]$dev == "yes") txt_install <- paste0(txt_install, "@dev")
+      
+      ## Install
+      devtools::install_github(txt_install)
+      
+    } else {
+      
+      ## Display
+      cat(" already installed.\n")
+      
+    }
+    
   }
-  
-  
-  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## rCharts
-  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  cat("Checking Package: rCharts ...")
-  
-  ## Check
-  suppressMessages(temp_check <- require("rCharts", 
-                                         character.only = TRUE,
-                                         warn.conflicts = FALSE,
-                                         quietly = TRUE))
-  
-  ## Install if needed
-  if (temp_check) {
-    cat(" already installed.\n")
-  } else {
-    cat(" not there ... now installing ...\n") 
-    suppressMessages(devtools::install_github("rCharts", "ramnathv", ref = "dev"))
-  }
-  
-  
-  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## slidify
-  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  cat("Checking Package: slidify ...")
-  
-  ## Check
-  suppressMessages(temp_check <- require("slidify", 
-                                         character.only = TRUE,
-                                         warn.conflicts = FALSE,
-                                         quietly = TRUE))
-  
-  ## Install if needed
-  if (temp_check) {
-    cat(" already installed.\n")
-  } else {
-    cat(" not there ... now installing ...\n") 
-    suppressMessages(devtools::install_github("slidify", "ramnathv", ref = "dev"))
-  }
-  
-  
-  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## slidifyLibraries
-  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  cat("Checking Package: slidifyLibraries ...")
-  
-  ## Check
-  suppressMessages(temp_check <- require("slidifyLibraries", 
-                                         character.only = TRUE,
-                                         warn.conflicts = FALSE,
-                                         quietly = TRUE))
-  
-  ## Install if needed
-  if (temp_check) {
-    cat(" already installed.\n")
-  } else {
-    cat(" not there ... now installing ...\n") 
-    suppressMessages(devtools::install_github("slidifyLibraries", "ramnathv", ref = "dev"))
-  }
-  
+    
 }
-
-
 
 
